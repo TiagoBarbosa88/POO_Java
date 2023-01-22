99b9c4e274d742fade454cc8cf5a204f1a21bfc4
TiagoBarbosa88 <sombr43@gmail.com>
2023-01-22 17:24:58 -0300

    construtores

--- POO Aula-2/src/Animains/Cachorro.java
+++ POO Aula-2/src/Animains/Cachorro.java
@@ -1,15 +1,75 @@
 package Animains;
 
 public class Cachorro {
-   public String nome;
-    public String cor;
-    public int altura;
-    public double peso;
-    public int tamanhoDoRabo;
+// Atributos
+   private String nome;
+    private String cor;
+    private int altura;
+    private double peso;
+    private int tamanhoDoRabo;
+    private String estadoDeEspirito;
 
-    public String estadoDeEspirito;
+// Construtor padrão
+    public Cachorro(){}
 
-    public void comer(){}
+// Construtor adicionados
+    public Cachorro(String nome, String cor, int altura, double peso, int tamanhoDoRabo, String estadoDeEspirito) {
+        this.nome = nome;
+        this.cor = cor;
+        this.altura = altura;
+        this.peso = peso;
+        this.tamanhoDoRabo = tamanhoDoRabo;
+        this.estadoDeEspirito = estadoDeEspirito;
+    }
+
+    // Metodos
+    public String getNome(){
+        return this.nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getCor(String marrom) {
+        return cor;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public void setTamanhoDoRabo(int tamanhoDoRabo) {
+        this.tamanhoDoRabo = tamanhoDoRabo;
+    }
+
+    public int getTamanhoDoRabo() {
+        return tamanhoDoRabo;
+    }
+
+    public String getEstadoDeEspirito() {
+        return estadoDeEspirito;
+    }
+
+
+
+    private void comer(){}
 
     public void latir(){
         System.out.println("Au Au");
@@ -20,13 +80,21 @@ public class Cachorro {
     }
 
     public String interagir(String acao) {
-        if(acao.equals("carinho")){
-            this.estadoDeEspirito = "feliz";
-        } else if(acao.equals("vai dormir")){
-            this.estadoDeEspirito = "bravo";
-        }else {
-            this.estadoDeEspirito = "neutro";
+
+        switch (acao){
+            case "carinho" : this.estadoDeEspirito = "feliz"; break;
+            case "vai dormir" : this.estadoDeEspirito = "bravo"; break;
+            case "pisar na patinha" : this.estadoDeEspirito = "triste"; break;
+            default: this.estadoDeEspirito = "neutro"; break;
         }
+
+//        if(acao.equals("carinho")){
+//            this.estadoDeEspirito = "feliz";
+//        } else if(acao.equals("vai dormir")){
+//            this.estadoDeEspirito = "bravo";
+//        }else {
+//            this.estadoDeEspirito = "neutro";
+//        }
         return estadoDeEspirito;
     }
 
--- POO Aula-2/src/Main.java
+++ POO Aula-2/src/Main.java
@@ -2,17 +2,22 @@ import Animains.Cachorro;
 
 public class Main {
     public static void main(String[] args) {
-        Cachorro cachorro1 = new Cachorro();
-        cachorro1.nome = "Titos";
-        cachorro1.cor = "Caramelo";
-        cachorro1.altura = 25;
-        cachorro1.peso = 7.5;
-        cachorro1.tamanhoDoRabo = 5;
 
-        //cachorro1.latir();
-        //System.out.println("O cahorro a " + cachorro1.pegar());
+        Cachorro cachorro1 = new Cachorro();
+        Cachorro cachorro2 = new Cachorro("Tito", "preto", 20, 7.3, 5,"nada");
+
+//        cachorro1.setNome("Rex");
+//        cachorro1.setCor("marrom");
+//        cachorro1.setPeso(7.5);
+//        cachorro1.setAltura(25);
+//        cachorro1.setTamanhoDoRabo(5);
+
+
+        System.out.println(cachorro2.getNome());
+        System.out.println(cachorro2.getPeso());
 
         System.out.println("O cachorro está " + cachorro1.interagir("carinho"));
+        System.out.println("O cachorro está " + cachorro1.interagir("pisar na patinha"));
         System.out.println("O cachorro está " + cachorro1.interagir("vai dormir"));
         System.out.println("O cachorro está " + cachorro1.interagir("nada"));
 
