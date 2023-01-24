a174eff6c9c6c34f10df69b25f831cd1b40bb264
TiagoBarbosa88 <sombr43@gmail.com>
2023-01-23 22:11:27 -0300

    Modelagem OO

--- POO Aula-2/src/Animains/Cachorro.java
+++ POO Aula-2/src/Animains/Cachorro.java
@@ -2,6 +2,8 @@ package Animains;
 
 public class Cachorro {
 // Atributos
+
+   static int numeroDeCachorros;
    private String nome;
     private String cor;
     private int altura;
@@ -20,9 +22,20 @@ public class Cachorro {
         this.peso = peso;
         this.tamanhoDoRabo = tamanhoDoRabo;
         this.estadoDeEspirito = estadoDeEspirito;
+
+        numeroDeCachorros ++;
     }
 
     // Metodos
+
+    public static int getNumeroDeCachorros() {
+        return numeroDeCachorros;
+    }
+
+    public static void setNumeroDeCachorros(int numeroDeCachorros) {
+        Cachorro.numeroDeCachorros = numeroDeCachorros;
+    }
+
     public String getNome(){
         return this.nome;
     }
@@ -98,4 +111,10 @@ public class Cachorro {
         return estadoDeEspirito;
     }
 
+    @Override
+    public String toString() {
+        return "Cachorro{" +
+                "nome='" + nome + '\'' +
+                '}';
+    }
 }
--- POO Aula-2/src/Main.java
+++ POO Aula-2/src/Main.java
@@ -3,8 +3,18 @@ import Animains.Cachorro;
 public class Main {
     public static void main(String[] args) {
 
+
+
         Cachorro cachorro1 = new Cachorro();
+
+        System.out.println(cachorro1.getNumeroDeCachorros());
+
         Cachorro cachorro2 = new Cachorro("Tito", "preto", 20, 7.3, 5,"nada");
+        System.out.println(cachorro2.getNumeroDeCachorros());
+        System.out.println(cachorro1.getNumeroDeCachorros());
+
+        Cachorro cachorro3 = new Cachorro("Rex", "preto", 20, 7.3, 5,"nada");
+
 
 //        cachorro1.setNome("Rex");
 //        cachorro1.setCor("marrom");
@@ -13,13 +23,15 @@ public class Main {
 //        cachorro1.setTamanhoDoRabo(5);
 
 
-        System.out.println(cachorro2.getNome());
-        System.out.println(cachorro2.getPeso());
+//        System.out.println(cachorro2.toString());
+//        System.out.println(cachorro3.toString());
 
-        System.out.println("O cachorro está " + cachorro1.interagir("carinho"));
-        System.out.println("O cachorro está " + cachorro1.interagir("pisar na patinha"));
-        System.out.println("O cachorro está " + cachorro1.interagir("vai dormir"));
-        System.out.println("O cachorro está " + cachorro1.interagir("nada"));
+        System.out.println("------------------------------");
+
+//        System.out.println("O cachorro está " + cachorro1.interagir("carinho"));
+//        System.out.println("O cachorro está " + cachorro1.interagir("pisar na patinha"));
+//        System.out.println("O cachorro está " + cachorro1.interagir("vai dormir"));
+//        System.out.println("O cachorro está " + cachorro1.interagir("nada"));
 
     }
 }
